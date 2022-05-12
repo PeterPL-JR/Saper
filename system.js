442a5f4017499f80f6ca46960be9c7173e10363b
PeterPL-JR <73580223+PeterPL-JR@users.noreply.github.com>
2022-05-12 12:57:40 +0200

    Add files via upload

--- system.js
+++ system.js
@@ -8,13 +8,27 @@ var firstX = -1;
 var firstY = -1;
 
 const size = tileSize * mapSize;
+const boundsPositions = [];
+
 const tiles = [];
 const bombs = [];
+
+const colors = [
+    "blue", "green", "red", "darkblue",
+    "white", "white", "white", "white"
+];
 
 container.style.width = size + "px";
 container.style.height = size + "px";
 
 function createTiles() {
+    for (var x = -1; x <= 1; x++) {
+        for (var y = -1; y <= 1; y++) {
+            if (x == 0 && y == 0) continue;
+            boundsPositions.push([x, y]);
+        }
+    }
+
     for (var x = 0; x < mapSize; x++) {
         tiles[x] = [];
         for (var y = 0; y < mapSize; y++) {
@@ -24,9 +38,6 @@ function createTiles() {
             container.appendChild(div);
 
             tiles[x][y] = {
-                xPos: x,
-                yPos: y,
-
                 obj: div,
                 bounds: 0,
                 bomb: false
@@ -40,7 +51,11 @@ function createBombs() {
         var randX = getRandom(0, 8);
         var randY = getRandom(0, 8);
 
-        if(randX == firstX && randY == firstY) {
+        console.log("FirstX: ", firstX, ", X: ", randX);
+        console.log("FirstY: ", firstY, ", Y: ", randY);
+        console.log("\n");
+
+        if (randX == firstX && randY == firstY) {
             i--;
             continue;
         }
@@ -53,7 +68,7 @@ function createBombs() {
         bombs.push([randX, randY]);
     }
 
-    for(var array of bombs) {
+    for (var array of bombs) {
         var x = array[0];
         var y = array[1];
         tiles[x][y].bomb = true;
@@ -66,17 +81,40 @@ function createBombs() {
     }
 }
 
+function createBounds() {
+    for (var x = 0; x < mapSize; x++) {
+        for (var y = 0; y < mapSize; y++) {
+            var tile = tiles[x][y];
+            if (tile.bomb) continue;
+
+            var bombsCounter = 0;
+            for (var array of boundsPositions) {
+                var xx = x + array[0];
+                var yy = y + array[1];
+
+                if(xx < 0 || yy < 0 || xx >= mapSize || yy >= mapSize) continue;
+                if (tiles[xx][yy].bomb) bombsCounter++;
+            }
+            if(bombsCounter != 0) {
+                tile.obj.innerHTML = bombsCounter;
+                tile.obj.className = "tile tile-shown bordered";
+            } 
+        }
+    }
+}
+
 function clickTile(x, y) {
     var tile = tiles[x][y];
     if (!firstClicked) {
         firstX = x;
-        firstY = x;
+        firstY = y;
 
         firstClicked = true;
         createBombs();
+        createBounds();
     }
 
-    if(tile.bomb) {
+    if (tile.bomb) {
         tile.obj.innerHTML = "<div class='bomb'></div>"
     }
     tile.obj.className = "tile tile-shown";
